acf5bb57ae15951f02af9e4336cabffcaf2eb5c5
Samik <mailtoskittur@gmail.com>
2018-10-08 22:28:39 +0530

    gitignore

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
+require('dotenv/config');
+require('../server');
 module.exports = {
     //database:'mongodb://localhost:27017/students',
-    database:'mongodb://samik:samik123@ds121686.mlab.com:21686/students',
-    secret : 'yoursecret'
+    database: process.env.MDB_ENV,
+    secret: process.env.SECRET_CODE
 }
