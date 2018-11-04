f63a62f26746cd4b53b1fd1fab57bf2ed9dd6bfe
Samik <mailtoskittur@gmail.com>
2018-11-04 22:28:20 +0530

    git ignore

--- config/db.js
+++
@@ -1,7 +0,0 @@
-require('dotenv/config');
-const db = process.env.MDB_ENV;
-module.exports = {
-    //database:'mongodb://localhost:27017/students',
-    database: db,
-    secret: process.env.SECRET_CODE
-}
